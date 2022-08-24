445d2b227cb19965b01e493c4cd5c2f8896f8b20
Gabriel Caroni <gabriel.caroni@personal.com.py>
2022-08-24 09:26:24 -0400

    Refactor y comentarios

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const SHEET_ID = "1SY8CrCyX_yNpdURlXxWup_pQWJ59X9vnOGlN0yE2aow";
 
 const ACCESS_TOKEN =
-  "ya29.A0AVA9y1uHVcjGMnVX8LhE1Ixynt76M57qRaJFMU786xB6lFxLmNsDxwfru6_cuaPN6-3gTOqvjerilEH-sqdsXbxcZvNAtG0n3KWI3kHup9xQYtZ3A3L-4ztekNDWLr1gru7CfZA8wln44XhvSzW-BGkswlyhaCgYKATASATASFQE65dr88T-uUUhm6zt9IyodYrLKKA0163";
+  "ya29.A0AVA9y1s92zHK1z5VDfnNOniFq9l-O7zXjcnqjy41IY_SpXw8oI-IBbj8AoD23_n5zZM16R77VtgMQpdD3ypsbIuwzDgWH_l_ZbLNeOtTKX5iHCq2cuh7V-gPC09fV-hAZlJxXUk3Zs2CxGTQuohqWfn6Urj6aCgYKATASATASFQE65dr8y7vf7mUfgGE5UgzcQD8URA0163";
 
 fetch(
+  // Obtenemos los datos de la planilla, de la hoja hojaMenu, columnas A y B desde la segunda fila
   `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/hojaMenu!A2:B`,
   {
     headers: {
@@ -11,18 +12,47 @@ fetch(
       Authorization: `Bearer ${ACCESS_TOKEN}`,
     },
   }
+//esperamos el response
 ).then(function (response) {
+  //esperamos el json del response para poder utilizarlo
   response.json().then(function (data) {
     const values = data.values;
 
-    for (var i = 0; i < values.length; i++) {
-        console.log(i);
-        var lista = document.getElementById("lista-menu");
-        var producto = document.createElement("div");
-        producto.className = "menu-item";
+    // Obtenemos el elemento del dom
+    const lista = document.getElementById("lista-menu");
 
-        producto.innerHTML = `<div class="index-item"><span class="item indice">${i+1}</span></div><span class="item producto">${values[i][0]}</span><span class="item precio">${values[i][1]}</span>`;
- 
+    for (var i = 0; i < values.length; i++) {
+
+        // Div que va a contener los datos del producto
+        const producto = document.createElement("div");
+        producto.className =  "menu-item";
+
+        // El número de producto
+        const itemIndice = document.createElement("span");
+        itemIndice.className = "item indice";
+        itemIndice.innerHTML = i + 1;
+
+        // Nombre del producto
+        const itemProducto = document.createElement("span");
+        itemProducto.className = "item producto";
+        itemProducto.innerHTML = values[i][0]; 
+
+        // Precio
+        const itemPrecio = document.createElement("span");
+        itemPrecio.className = "item precio";
+        itemPrecio.innerHTML = values[i][1];
+
+        // Div que contiene el número de producto (itemIndex)
+        const divIndice = document.createElement("div");
+        divIndice.className = "index-item";
+        divIndice.appendChild(itemIndice);
+
+        // Agregamos todos los elementos al div de producto
+        producto.appendChild(divIndice);
+        producto.appendChild(itemProducto);
+        producto.appendChild(itemPrecio);
+
+        // Agregamos el producto a la lista
         lista.appendChild(producto);
      }
   });
